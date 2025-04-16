1dafb056536b94e52bd2f75cba634bdbcdf51fc4
ItsTrudeauver <smz.osaragi@gmail.com>
2025-04-17 01:43:30 +0700

    adding thumbnails

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Helmet>
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="Ar2chive – Community Book Repertoire" />
-        <meta property="og:description" content="A sleek archive of community-recommended books, indexed by category and importance." />
+        <meta property="og:description" content="archive of community-recommended books." />
         <meta property="og:image" content="https://i.pinimg.com/736x/12/1c/ee/121cee02940154b9f2b66c82fe2da874.jpg" />
         <meta property="og:url" content="https://ar2chive.vercel.app" />
         <meta property="og:type" content="website" />
@@ -18,7 +18,7 @@ const App = () => {
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Ar2chive – Community Book Repertoire" />
-        <meta name="twitter:description" content="A sleek archive of community-recommended books." />
+        <meta name="twitter:description" content="archive of community-recommended books." />
         <meta name="twitter:image" content="https://i.pinimg.com/736x/12/1c/ee/121cee02940154b9f2b66c82fe2da874.jpg" />
 
         {/* Optional: Set title and favicon */}
